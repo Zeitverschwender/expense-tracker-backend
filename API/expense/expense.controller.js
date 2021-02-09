3806feb2ea7fed56add4921cb6ae4ed8f4daced5
SamehAmnoun <samehamnoun@gmail.com>
2021-02-09 15:41:14 +0200

    refactor: refactored the document and remoived depreacation

--- API/expense/expense.controller.js
+++ API/expense/expense.controller.js
@@ -1,66 +1,68 @@
 const Expense = require("../../db/models/expense");
 
 const getAllExpenses = async (req, res, next) => {
-    try{
-        const allItems = await Expense.find();
-        res.json(allItems);
-    }
-    catch (e){
-        return next(e)
-    }
-}
+  try {
+    const allItems = await Expense.find();
+    res.json(allItems);
+  } catch (e) {
+    next(e);
+  }
+};
 const getSpecificExpenses = async (req, res, next) => {
-	try{
-			const currentItem = await Expense.findById(req.params.id);
-			res.json(currentItem);
-	}
-	catch (e){
-			return next(e)
-	}
-}
+	const expenseId = req.params.id;
+  try {
+    const currentItem = await Expense.findById(expenseId);
+    res.json(currentItem);
+  } catch (e) {
+    next(e);
+  }
+};
 const addExpenses = async (req, res, next) => {
-	try{
+	const expense = req.body;
+  try {
     const newItem = new Expense(req.body);
     await newItem.save();
     res.status(201).json({
-			status: "Success",
-			message: "Expense created successfuly",
-			newItem
-		});
-	}
-	catch (e){
-		return next(e)
-	}
-}
+      status: "Success",
+      message: "Expense created successfuly",
+      newItem,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
 const updateExpenses = async (req, res, next) => {
-	try{
-    const updatedExpense = await Expense.findByIdAndUpdate(req.params.id, req.body,);
+	const expenseId = req.params.id;
+  try {
+    const expenseToUpdate = await Expense.findByIdAndUpdate(
+      expenseId,
+      req.body,
+			{useFindAndModify:false}
+    );
     res.json({
       status: "Success",
-      message: "Expense updated successfuly",
-			updatedExpense
+      message: "Expense updated successfuly"
     });
-	}
-	catch (e){
-		return next(e);
-	}
-}
+  } catch (e) {
+    next(e);
+  }
+};
 const deleteExpenses = async (req, res, next) => {
-	try{
-		await Expense.remove({_id: req.params.id});
-		res.json({
+	const expenseId = req.params.id;
+  try {
+    await Expense.deleteOne({ _id: expenseId });
+    res.json({
       status: "Success",
       message: "Expense deleted successfuly",
     });
-	}
-	catch (e) {
-		return next(e);
-	}
-}
+  } catch (e) {
+    return next(e);
+  }
+};
 module.exports = {
   getAllExpenses,
-	getSpecificExpenses,
-	addExpenses,
-	updateExpenses,
-	deleteExpenses
-}
+  getSpecificExpenses,
+  addExpenses,
+  updateExpenses,
+  deleteExpenses,
+};
