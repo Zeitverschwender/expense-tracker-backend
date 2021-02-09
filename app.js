26006f0bf847dbd3992354fefccf3d4999d0ab15
SamehAmnoun <samehamnoun@gmail.com>
2021-02-09 15:39:16 +0200

    fix: add express.json middleware and imported routes for testing

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 require('./db/connect');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json())
+
+//Routes
+const expenseRoute = require("./API/expense/expense.routes");
+
+app.use("/expense", expenseRoute);
 
 app.listen(port, async () => {
   console.log(`running on port: ${port}`);
