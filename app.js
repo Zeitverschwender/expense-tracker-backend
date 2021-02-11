9a5827c307be29bcc333628092af4e6a505aac05
SamehAmnoun <samehamnoun@gmail.com>
2021-02-11 16:23:30 +0200

    feat: added cookie-parser

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-require("dotenv").config();
-
-const cors = require("cors");
-
 const express = require("express");
-require("./db/connect");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const errorHandler = require("./middlewares/errorHandler");
+const authMiddleware = require("./middlewares/auth");
+
+require("dotenv").config();
+require("./db/connect");
+
+
+
 const app = express();
 const port = process.env.PORT || 3000;
+
 
 //Bodyparser
 app.use(express.json());
@@ -19,15 +24,21 @@ app.use(
   })
 );
 
+//Cookie-Parser
+app.use(cookieParser());
+
 //Routes
 const expenseRoute = require("./API/expense/expense.routes");
 const categoryRoute = require("./API/category/category.routes");
 const authRoute = require("./API/authentication/auth.routes");
 
-app.use("/expenses", expenseRoute);
+app.use("/expenses", authMiddleware.authenticateToken, expenseRoute);
 app.use("/categories", categoryRoute);
 app.use("/auth", authRoute);
 
+app.get("/loggedin", async (req, res) => {
+  res.send("Logged In Successfully.")
+})
 app.use(errorHandler);
 app.listen(port, async () => {
   console.log(`running on port: ${port}`);
