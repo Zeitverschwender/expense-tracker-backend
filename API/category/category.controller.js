90988b42386d7250d8c3ea76fff4a26c0341b735
SamehAmnoun <samehamnoun@gmail.com>
2021-02-13 13:47:25 +0200

    feat: categories endpoint now gets data from logged in user

--- API/category/category.controller.js
+++ API/category/category.controller.js
@@ -1,62 +1,72 @@
-const { nextTick } = require("process");
+const DatabaseHelpers =  require("../../utils/DatabaseHelpers");
 const Category = require("../../db/models/category");
 const Exception = require('../../utils/Exception');
 
+const getCategory = async (req, res, next) => {
+  const catgeoryId = req.params.id;
+  try {
+    const currUser = await DatabaseHelpers.getUserFromId(req.user);
+    const category = currUser.categories.id(catgeoryId);
+    res.json(category);
+  } catch (e) {
+    next(new Exception(400, e.message))
+  }
+};
+
 const getAllCategories = async (req, res, next) => {
   try {
-    const categories = await Category.find({});
+    const currUser = await DatabaseHelpers.getUserFromId(req.user);
+    const categories = await currUser.categories;
     res.json(categories);
   } catch (e) {
     next(new Exception(400, e.message))
   }
 };
+
 const addCategory = async (req, res, next) => {
+  const category = req.body;
   try {
-    const category = await Category.create(req.body);
+    const currUser = await DatabaseHelpers.getUserFromId(req.user);
+    const newCategory = await new Category(category);
+    currUser.categories.push(newCategory);
     res.status(201).json({
       status: "Success",
       message: "Category created successfuly",
-      category,
+      newCategory,
     });
   } catch (e) {
     next(new Exception(400,e.message));
   }
 };
 
-const getCategory = async (req, res, next) => {
+const updateCategory = async (req, res) => {
   const catgeoryId = req.params.id;
+  const updatedCategoryFields = req.body;
   try {
-    const category = await Category.findById(catgeoryId);
-    res.json(category);
-  } catch (e) {
-    next(new Exception(400, e.message))
-  }
-};
-const deleteCategory = async (req, res, next) => {
-  const catgeoryId = req.params.id;
-  try {
-    await Category.deleteOne({ _id: catgeoryId });
+    const currUser = await DatabaseHelpers.getUserFromId(req.user);
+    const categoryToUpdate = currUser.categories.id(catgeoryId);
+    categoryToUpdate.set(updatedCategoryFields);
+    await currUser.save();
     res.json({
       status: "Success",
-      message: "Category deleted successfuly",
+      message: "Category updated successfuly",
+      categoryToUpdate,
     });
   } catch (e) {
     next(new Exception(400, e.message))
   }
 };
-const editCategory = async (req, res) => {
+
+
+const deleteCategory = async (req, res, next) => {
   const catgeoryId = req.params.id;
-  const updatedCategoryFields = req.body;
   try {
-    const category = await Category.findByIdAndUpdate(
-      catgeoryId,
-      updatedCategoryFields,
-      {useFindAndModify:false}
-    );
+    const currUser = await DatabaseHelpers.getUserFromId(req.user);
+    currUser.categories.id(catgeoryId).remove();
+    await currUser.save()
     res.json({
       status: "Success",
-      message: "Category updated successfuly",
-      category,
+      message: "Category deleted successfuly",
     });
   } catch (e) {
     next(new Exception(400, e.message))
@@ -67,6 +77,6 @@ module.exports = {
   getAllCategories,
   addCategory,
   getCategory,
+  updateCategory,
   deleteCategory,
-  editCategory,
 };
--- API/category/category.routes.js
+++ API/category/category.routes.js
@@ -4,7 +4,7 @@ const categoryController = require("./category.controller");
 router.get("/", categoryController.getAllCategories);
 router.post("/", categoryController.addCategory);
 router.get("/:id", categoryController.getCategory);
+router.patch("/:id", categoryController.updateCategory);
 router.delete("/:id", categoryController.deleteCategory);
-router.patch("/:id", categoryController.editCategory);
 
 module.exports = router;
