c2ea2abba8d21ab2a3e2839d34201258f5a01fc3
SamehAmnoun <samehamnoun@gmail.com>
2021-02-14 14:18:53 +0200

    feat: authenticateToken checks if token is in database

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,14 @@
 const jwt = require("jsonwebtoken");
 const Exception = require("../utils/Exception");
+const Token = require("../db/models/token");
 
-const authenticateToken = (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
   const token = req.cookies.token;
   if(token == null) return next(new Exception(401, "There is no token sent."));
+  const tokenDocument =  await Token.findById(token);
+  if(tokenDocument == null){
+    return next(new Exception(403, "Token has expired."));
+  }
   jwt.verify(token, process.env.JWT_SECRET,(err,user) => {
     if (err) return next(new Exception(403, "Token no longer valid."));
     req.user = user;
