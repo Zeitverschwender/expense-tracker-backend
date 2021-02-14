1985d47dbb49c5b16c6623a46587aa22f06dc7fc
SamehAmnoun <samehamnoun@gmail.com>
2021-02-14 16:58:10 +0200

    fix: added missing try catch block and returns proper errors

--- API/user/user.controller.js
+++ API/user/user.controller.js
@@ -1,13 +1,22 @@
 const DatabaseHelpers =  require("../../utils/DatabaseHelpers");
+const Exception = require('../../utils/Exception');
 
 const getGooglePhoto = async (req, res) => {
-	const currUser =  await DatabaseHelpers.getUserFromId(req.user);
-  res.send(currUser.profile_picture_url);
+	try{
+		const currUser =  await DatabaseHelpers.getUserFromId(req.user);
+		res.send(currUser.profile_picture_url);
+	} catch (e){
+		next(new Exception(400, e.message))
+	}
 }
 
 const getUsername = async (req, res) => {
-	const currUser =  await DatabaseHelpers.getUserFromId(req.user);
-	res.send(currUser.name);
+	try{
+		const currUser =  await DatabaseHelpers.getUserFromId(req.user);
+		res.send(currUser.name);
+	} catch(e){
+		next(new Exception(400, e.message))
+	}
 }
 
 module.exports = {
